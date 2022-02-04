6fb052d4e19ec382d197a35225f845a5c84bf70d
Dominik Sawicki <dominik.sawicki@student.put.poznan.pl>
2022-02-05 00:19:16 +0100

    Add user authorization

--- app.py
+++ app.py
@@ -1,14 +1,85 @@
+import sqlite3
+import os
+import bcrypt
+
+
+class User():
+    def __init__(self, username) -> None:
+        self.username = username
+        self.words = None
+    
+    def load_words(self, filename: str): # load word list
+        pass
+
+    def create_log(self): # create new log
+        pass
+
+    def load_log(self): # load previous logs (if exist)
+        pass
 
 def init_db():
-    pass
+    with sqlite3.connect('users.db') as con:
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL)''')
+        con.commit()
 
-def create_user():
-    pass
+def create_user(username: str, password: str):
+    hashed = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt(13))
+    with sqlite3.connect('users.db') as con:
+        cur = con.cursor()
+        try:
+            cur.execute('''INSERT INTO users (username, password)
+            VALUES (?, ?)''', (username, hashed))
+            con.commit()
+        except sqlite3.Error as er:
+            return ' '.join(er.args)
+        return True
 
-def login():
-    pass
+def login(username: str, password: str):
+    with sqlite3.connect('users.db') as con:
+        cur = con.cursor()
+        cur.execute('''SELECT * FROM users WHERE username = ?''', (username,))
+        user = cur.fetchone()
+        if user and bcrypt.checkpw(password.encode('utf8'), user[2]):
+            return user
+        else:
+            return False
 
-
+def clear_CLI():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
 
 if __name__ == '__main__':
-    pass
+    init_db()
+
+    while True:
+        clear_CLI()
+        option = input('Type command: ')
+
+        if option == '':
+            pass
+        elif option == 'create':
+            username = input('Type your username: ')
+            password = input('Type password: ')
+            repeat = input('Please repeat your password: ')
+            if password == repeat:
+                if (msg := create_user(username, password)) == True:
+                    input('Account successfully created!')
+                else:
+                    input(msg)
+            else:
+                input('Your passwords don\'t match!')
+        elif option == 'login':
+            username = input('Type your username: ')
+            password = input('Type password: ')
+            if user := login(username, password):
+                input(user)
+            else:
+                input('Wrong username or password!')
+        else:
+            print('Wrong option! Type help to list available commands.')
--- readme.md
+++ readme.md
@@ -4,4 +4,10 @@ A simple CLI application for learning words.
 
 ## Installation
 
+1. Program do nauki słówek języka obcego
 
+Minimalne wymagania (na ocenę maksymalnie 3.5): 
+
+Utwórz program do nauki obcych słówek. Program ma posiadać system zarządzania kontami użytkownika (każdy użytkownik ma swoje konto chronione hasłem, na którym przechowywane są jego postępy). Program powinien posiadać interfejs tekstowy, w którym użytkownik wskazuje plik ze słówkami do nauki. Następnie program importuje dane i wprowadza je do słownika. W kolejnym etapie program przepytuje użytkownika z wiedzy zawartej w pliku ze słówkami. Program powinien wyliczać za ile dni użytkownik powinien powtórzyć naukę - jeśli odpowiedział dobrze - za 7 dni, jeśli źle - za 0 dni. Program tworzy nowy plik, który będzie zawierać informacje na temat daty ostatniej sesji nauki, id przerobionych słówek oraz za ile dni należy odbyć powtórkę. Program dodatkowo ma importować log z poprzedniej sesji aby odpytywać użytkownika z zaplanowanej powtórki. W ramach projektu należy wykorzystać wszystkie poznane mechanizmy programowania. 
+
+Dodatkowe funkcjonalności programu oraz zaawansowane mechanizmy (funkcje, klasy), sporządzenie opisu/dokumentacji zwiększają ocenę za realizację projektu.
